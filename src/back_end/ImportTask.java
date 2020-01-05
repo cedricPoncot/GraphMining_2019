f1da371dd210dbd6fd70f6c1d1a4c1e97aa9345e
Imene Lydia Kerboua <imenelydiaker@users.noreply.github.com>
2020-01-05 18:48:34 +0100

    ajout de commentaires au code

--- src/back_end/ImportTask.java
+++ src/back_end/ImportTask.java
@@ -1,47 +1,52 @@
 package back_end;
 
 import javafx.concurrent.Task;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class ImportTask extends Task<ArrayList> {
-    private String cheminCsv;
-    private int nbLignes;
+    /* ImportTask étend la classe abstraite Task<V>, elle doit alors implémenter la fonction call() de cette dernière.
+     * Cette tâche sera executée dans un Thread séparé pour ne pas bloquer le Thread de la GUI (JavaFX Application Thread).
+     * Cette tâche retourne uen ArrayList de Tweets créés à partir des données du fichier lu.*/
 
-    public ImportTask(String cheminCsv, int nbLignes){
+    //ATTRIBUTS
+    private String cheminCsv; //Chemin du fichier à lire
+    private int nbTweetsTotal; //Nombre de lignes du fichier (pour afficher la progression de la tâche)
+
+    //CONSTRUCTEUR
+    public ImportTask(String cheminCsv, int nbTweetsTotal){
         this.cheminCsv = cheminCsv;
-        this.nbLignes = nbLignes;
+        this.nbTweetsTotal = nbTweetsTotal;
     }
 
     @Override
     protected ArrayList call() throws Exception {
-        BufferedReader br=null;
-        String line= "";
-        ArrayList<Tweet> tweets = new ArrayList<>();
+        BufferedReader br=null; //Buffer pour la lecture du fichier
+        String line= ""; //Chaîne contenant la ligne lue
+        ArrayList<Tweet> tweets = new ArrayList<>(); //Liste des tweets qui sera remplie avec les tweets du fichier
 
         try {
             br = new BufferedReader(new FileReader(cheminCsv));
-            int compteurLigne = 0;
-            while ((line = br.readLine()) != null) {
-                String[] data = line.split("\t");
-                compteurLigne++;
+            int nbTweetsLus = 0; //Compte le nombre de tweets déjà lus
+            while ((line = br.readLine()) != null) { //Tant que le fichier n'est pas vide
+                String[] data = line.split("\t"); //Lire la ligne du fichier
+                nbTweetsLus++;
                 if(data.length==5)
                     tweets.add(new Tweet(data[0], data[1], data[2], data[3], data[4]));
                 else {
                     if (data.length == 4)
                         tweets.add(new Tweet(data[0], data[1], data[2], data[3]));
                 }
-                this.updateProgress(compteurLigne, nbLignes);
-                charger(compteurLigne);
+                this.updateProgress(nbTweetsLus, nbTweetsTotal); //Actualiser le progrès de la tâche
+                message(nbTweetsLus); //Actualiser le message à afficher
             }
         }
         catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        }
+        finally { //Fermer le buffer à la fin de la lecture ou s'il y a une erreur lors de la lecture
             if(br!=null){
                 try{
                     br.close();
@@ -51,12 +56,13 @@ public class ImportTask extends Task<ArrayList> {
                 }
             }
         }
-        return tweets;
+        return tweets; //Retourner une ArrayList des tweets récupérés à partir du fichier lu
     }
 
-    private void charger(int ligneCourante) throws InterruptedException {
-        this.updateMessage(ligneCourante+" sur "+nbLignes+" tweets");
-        //Thread.sleep(0,1);
+    //Message à afficher au cours de l'exécution de la tâche
+    private void message(int nbTweetsLus) throws InterruptedException {
+        //Affiche le nombre de tweets importés sur le nombre de tweets total
+        this.updateMessage(nbTweetsLus+" sur "+nbTweetsTotal+" tweets");
     }
 
 }
--- src/frontEnd/Controller.java
+++ src/frontEnd/Controller.java
@@ -4,10 +4,7 @@ import back_end.Centrality;
 import back_end.Graphe;
 import back_end.ImportTask;
 import back_end.Tweet;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXProgressBar;
-import com.jfoenix.controls.JFXRadioButton;
-import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
@@ -23,8 +20,14 @@ import java.text.DecimalFormat;
 import java.util.TreeSet;
 
 public class Controller {
+    /* Calsse Controller : cette classe relie le code du backend à l'interface utilisateur.
+     * Elle modifie aussi les valeurs des éléments de la GUI.
+     * Le positionnement des éléments de la GUI est détaillé dans le fichier FXML.
+     * Le style de ces éléments est détaillé dans la feuille de style : style.css */
 
-    /***********************************VARIABLES FXML***********************************/
+    /*****************************************************************************************/
+    /***************************************ELEMENTS FXML*************************************/
+    /*****************************************************************************************/
     @FXML
     JFXRadioButton rdClimat, rdFoot;
 
@@ -55,7 +58,12 @@ public class Controller {
     @FXML
     HBox menuBox;
 
-    /***********************************AUTRES VARIABLES UTILES***********************************/
+    @FXML
+    JFXTextField txtNbUsersCentraux;
+
+    /*****************************************************************************************/
+    /*********************************AUTRES VARIABLES UTILES*********************************/
+    /*****************************************************************************************/
     private byte dataset = 0; //1: climat 2 : foot
     private Graphe g=null;
     private String descriptionFoot = "Ce jeu de données met à votre disposition :\n- 899597 tweets.\n- Les tweets sélectionnés contiennent les mots clés suivants : " +
@@ -77,72 +85,62 @@ public class Controller {
             "\t\tTexte : contient le contenu du tweet, hashtag compris.\n" +
             "\t\tRetweeterID : représente le nom d'utilisateur de la personne qui a retweeté le tweet courant.";//description du jeu de données Climat
 
-    /***********************************FONCTIONS UTILES***********************************/
-
+    /*****************************************************************************************/
+    /*************************************FONCTIONS UTILES************************************/
+    /*****************************************************************************************/
     //Action du bouton importer
     public void importerDonnees() {
+        //Modification des propriétés des éléments de la page d'import des données
         bImport.setDisable(true);
         bAnnuler.setDisable(false);
         menuBox.setDisable(true);
-        String path = "";
-        int nbLignes=0;
+
+        String path = ""; //Chemin du fichier
+        int nbTweets=0; //Nombre de tweets du fichier
 
         //sélection du dataset
         if(dataset==1){
             path = "src/data/climat.txt";
-            nbLignes = 1977769;
+            nbTweets = 1977769;
         }
         else{
             if(dataset==2){
                 path ="src/data/foot.txt";
-                nbLignes= 899597;
+                nbTweets= 899597;
             }
             else
                 errorDialog("Dataset non sélectionné !", "Veuillez sélectionner un dataset à importer.");
         }
 
-        //Création du graphe et import des données
         try {
-            g = new Graphe(path, nbLignes);
+             //Création du graphe
+            g = new Graphe(path, nbTweets);
 
             //Import des données
+            ImportTask importTask = new ImportTask(path, nbTweets); // création de la tâche d'import des données
+
+            //Modification des propriétés des éléments de la page d'import des données
             importProgress.setVisible(true);
             importProgress.setProgress(0);
             lbProgressStatus.setVisible(true);
-
-            ImportTask importTask = new ImportTask(path, nbLignes); // création de la tâche d'import des données
-
             importProgress.progressProperty().unbind();
-            importProgress.progressProperty().bind(importTask.progressProperty());
+            importProgress.progressProperty().bind(importTask.progressProperty()); //Relier la progress bar à la progression de la tâche d'import des données
             lbProgressStatus.textProperty().unbind();
-            lbProgressStatus.textProperty().bind(importTask.messageProperty());
+            lbProgressStatus.textProperty().bind(importTask.messageProperty()); //Relier e message à afficher à la progression de la tâche d'import des données
 
             new Thread(importTask).start(); // création et lancement du thread d'import des données
 
             //Lorsque la tâche d'import est terminée :
             importTask.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, event -> {
-                g.bd.setTweets(importTask.getValue());//quand l'import est terminé : on récupère le résultat
-                importProgress.progressProperty().unbind();
-                importProgress.setProgress(0);
-                importProgress.setVisible(false);
-                lbProgressStatus.textProperty().unbind();
-                lbProgressStatus.setText("Chargement des données terminé.");
-                bImport.setDisable(false);
-                menuBox.setDisable(false);
-                bAnnuler.setDisable(true);
+                g.bd.setTweets(importTask.getValue());//Quand l'import est terminé : on récupère le résultat
+                importDefaultPane("Chargement des données terminé."); //Retour à la page par défaut
                 informationDialog("Données importées !", "Vous pouvez à présent afficher les données, afficher les statistiques ou faire du Clustering.");
             });
 
             //Annulation de l'import
             bAnnuler.setOnAction(event -> {
-                bImport.setDisable(false);
-                bAnnuler.setDisable(true);
-                menuBox.setDisable(false);
                 importTask.cancel(true);
-                importProgress.progressProperty().unbind();
-                lbProgressStatus.textProperty().unbind();
-                lbProgressStatus.setText("Chargement des données annulé.");
-                importProgress.setProgress(0);
+                importDefaultPane("Chargement des données annulé."); //Retour à la page par défaut
             });
 
         } catch (FileNotFoundException e) {
@@ -151,13 +149,15 @@ public class Controller {
 
     }
 
-
-    //Calculs : volume, diamètre, ordre etc.
-    public void calculs(){
+    //Action du pane des calculs : calcul du volume, diamètre, ordre etc.
+    private void calculs(){
         if(g!=null) {
+            //Modification des propriétés des éléments de la page de calcul des statistiques
             menuBox.setDisable(true);
             pnCalculsProgress.setVisible(true);
-            Task statsTask = new Task() { //Création de la tache de calcul
+
+            //Classe  anonyme : création de la tâche qui fait les calculs
+            Task calculsTask = new Task() {
                 @Override
                 protected Object call() {
                     g.bd.calculs();
@@ -165,56 +165,22 @@ public class Controller {
                 }
             };
 
+            //Modification des propriétés des éléments de la page de calcul des statistiques
             calculsProgress.setProgress(0);
             calculsProgress.progressProperty().unbind();
-            calculsProgress.progressProperty().bind(statsTask.progressProperty());
+            calculsProgress.progressProperty().bind(calculsTask.progressProperty()); //Relier la progress bar à la progression de la tâche de calcul
 
-            new Thread(statsTask).start(); //Lancer le thread des calculs
+            new Thread(calculsTask).start(); //Lancer le thread des calculs
 
             //A la fin des calculs :
-            statsTask.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, event -> {
-                if(dataset==1)
-                    lbSelectedDataset.setText("Climat");
-                else
-                    lbSelectedDataset.setText("Foot");
-                DecimalFormat df = new DecimalFormat("0.00");
-                lbOrdre.setText(String.valueOf(g.bd.getOrdre()));
-                lbDegreMoy.setText(String.valueOf(df.format(g.bd.getDegreeMoyen())));
-                lbVolume.setText(String.valueOf(g.bd.getVolume()));
-                if (g.bd.getDiametre() == Double.POSITIVE_INFINITY) lbDiametre.setText("+∞");
-                else lbDiametre.setText(String.valueOf(g.bd.getDiametre()));
-                setTab(g.bd.UserCentraux());
-                calculsProgress.progressProperty().unbind();
-                calculsProgress.setProgress(0);
-                pnCalculsProgress.setVisible(false);
-                menuBox.setDisable(false);
+            calculsTask.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED, event -> {
+                afficherCalculs(); //Afficher les résultats des caculs effectués
+                afficherUsersCentraux(g.bd.UserCentraux()); //Afficher les utilisateurs centraux
+                calculsDefaultPane(); //Retour aux paramètres par défaut de la page
             });
         }
         else
             errorDialog("Données non importées !", "Veuillez importer les données avant de procéder aux calculs.");
-    }
-
-    //Slection du ejeu de données
-    public void selectedDataset(){
-        if(rdClimat.isSelected()) {
-            dataset = 1;
-            txtDescriptionDonnees.setText(descriptionClimat);
-        }
-        else {
-            dataset = 2;
-            txtDescriptionDonnees.setText(descriptionFoot);
-        }
-    }
-
-    //Affichage des users centraux
-    public void setTab(TreeSet<Centrality> tab) {
-        ObservableList<Centrality> usersCentraux = FXCollections.observableArrayList(tab);
-        table.setItems(usersCentraux);
-    }
-
-    public void afficherTweets(){
-        ObservableList<Tweet> liste = FXCollections.observableArrayList(g.bd.getTweets());
-        tableAffichage.setItems(liste);
     }
 
     //Affichage du graphe
@@ -243,7 +209,72 @@ public class Controller {
             errorDialog("Données non importées !", "Veuillez importer les données avant de procéder aux calculs.");
     }*/
 
+    /*****************************************************************************************/
+    /**********************FONCTIONS QUI MODIFIENT LES ELEMENTS DE LA GUI*********************/
+    /*****************************************************************************************/
+    private void importDefaultPane(String message){
+        //Page d'import par défaut
+        bImport.setDisable(false);
+        menuBox.setDisable(false);
+        bAnnuler.setDisable(true);
+        importProgress.progressProperty().unbind();
+        importProgress.setProgress(0);
+        importProgress.setVisible(false);
+        lbProgressStatus.textProperty().unbind();
+        lbProgressStatus.setText(message);
+    }
+
+    private void calculsDefaultPane(){
+        //Page de calculs par défaut
+        calculsProgress.progressProperty().unbind();
+        calculsProgress.setProgress(0);
+        pnCalculsProgress.setVisible(false);
+        menuBox.setDisable(false);
+    }
+
+    //Affichage des calculs sur la GUI
+    private void afficherCalculs(){
+        if(dataset==1)
+            lbSelectedDataset.setText("Climat");
+        else
+            lbSelectedDataset.setText("Foot");
+        DecimalFormat df = new DecimalFormat("0.00");
+        lbOrdre.setText(String.valueOf(g.bd.getOrdre()));
+        lbDegreMoy.setText(String.valueOf(df.format(g.bd.getDegreeMoyen())));
+        lbVolume.setText(String.valueOf(g.bd.getVolume()));
+        if (g.bd.getDiametre() == Double.POSITIVE_INFINITY)
+            lbDiametre.setText("+∞");
+        else
+            lbDiametre.setText(String.valueOf(g.bd.getDiametre()));
+    }
+    
+    //Sélection du jeu de données
+    public void selectedDataset(){
+        if(rdClimat.isSelected()) {
+            dataset = 1;
+            txtDescriptionDonnees.setText(descriptionClimat);
+        }
+        else {
+            dataset = 2;
+            txtDescriptionDonnees.setText(descriptionFoot);
+        }
+    }
+
+    //Affichage des users centraux
+    private void afficherUsersCentraux(TreeSet<Centrality> tab) {
+        ObservableList<Centrality> usersCentraux = FXCollections.observableArrayList(tab);
+        table.setItems(usersCentraux);
+    }
+
+    //Afficher les tweets
+    private void afficherTweets(){
+        ObservableList<Tweet> liste = FXCollections.observableArrayList(g.bd.getTweets());
+        tableAffichage.setItems(liste);
+    }
+
+    /*****************************************************************************************/
     /***********************************AFFCIHAGE DES PANES***********************************/
+    /*****************************************************************************************/
     //Pane d'import des données
     public void importPane(){
         pnCalculs.setVisible(false);
@@ -281,8 +312,11 @@ public class Controller {
             errorDialog("Données non importées !", "Veuillez importer les données avant de procéder aux calculs.");
     }
 
+    /****************************************************************************************/
     /***********************************BOITES DE DIALOGUE***********************************/
-    public void informationDialog(String header, String content){
+    /****************************************************************************************/
+    //Message d'information
+    private void informationDialog(String header, String content){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("INFORMATION");
         alert.setHeaderText(header);
@@ -290,7 +324,8 @@ public class Controller {
         alert.showAndWait();
     }
 
-    public void errorDialog(String header, String content){
+    //Message d'erreur
+    private void errorDialog(String header, String content){
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("ERREUR");
         alert.setHeaderText(header);
@@ -298,12 +333,16 @@ public class Controller {
         alert.showAndWait();
     }
 
-    /***********************************FONCTIONS FXML***********************************/
+    /*****************************************************************************************/
+    /*************************************FONCTIONS FXML**************************************/
+    /*****************************************************************************************/
+    //Initialisation des TableView de la GUI
     @FXML
     public void initialize() {
+        //Initialisation de la table d'affichage des utilisateurs centraux
         col1.setCellValueFactory(new PropertyValueFactory<Centrality, String>("nom"));
         col2.setCellValueFactory(new PropertyValueFactory<Centrality, String>("poids"));
-
+        //Initialisation de la table d'affichage des tweets
         col3.setCellValueFactory(new PropertyValueFactory<Tweet, String>("identifiant"));
         col4.setCellValueFactory(new PropertyValueFactory<Tweet, String>("tweeter"));
         col5.setCellValueFactory(new PropertyValueFactory<Tweet, String>("date"));
