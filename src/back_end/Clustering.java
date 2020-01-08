e2e53f1c663d690b730fb6a5d9aebf1cae8c7b96
Cédric <45012770+cedricPoncot@users.noreply.github.com>
2020-01-08 03:44:19 +0100

    a

--- src/back_end/Clustering.java
+++ src/back_end/Clustering.java
@@ -9,9 +9,12 @@ import com.mxgraph.view.mxStylesheet;
 import org._3pq.jgrapht.edge.DefaultEdge;
 import org._3pq.jgrapht.graph.Subgraph;
 import org.jgrapht.Graph;
+import org.jgrapht.ListenableGraph;
 import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.AsSubgraph;
+import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultListenableGraph;
+import org.jgrapht.graph.DirectedWeightedMultigraph;
 import sun.security.provider.certpath.Vertex;
 
 import javax.imageio.ImageIO;
@@ -19,60 +22,70 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Clustering{
 
-    private Graph grapheClustering;
+    private HashMap<String,Object> vertexGraphAdapter=new HashMap<>();
+
+    //Couleur des communautées
+    private final String[] couleurs={"FFFEB5","#CAF7C9","#FFC58B","#8BDFFF","#A58BFF","#FFB5B9","#B5FFEF"};
+    private final int cstCouleur=7;
+
     public Clustering(Graph g, TreeSet<Centrality>userCentraux, HashMap<String, HashMap<String,Integer>>baseLink){
-        TreeSet<String> noeudsFiltre= new TreeSet<>();
+        JGraphXAdapter<Vertex, DefaultEdge> graphAdapter =constructionGraph(userCentraux,baseLink);
+        afficherGraphe(graphAdapter);
+    }
+
+    public JGraphXAdapter<Vertex, DefaultEdge> constructionGraph( TreeSet<Centrality>userCentraux, HashMap<String, HashMap<String,Integer>>baseLink){
+        JGraphXAdapter<Vertex, DefaultEdge> graphAdapter = new JGraphXAdapter(new DirectedWeightedMultigraph(org.jgrapht.graph.DefaultEdge.class));
+        int cmp=0;
+        //Parcours des différents utilisateurs centraux
         for(Centrality c:userCentraux){
             String utilisateurCentral=c.getNom();
-            noeudsFiltre.add(utilisateurCentral);
+            Object v1;
+            if(cmp<cstCouleur)v1=graphAdapter.insertVertex(graphAdapter.getDefaultParent(),null,utilisateurCentral,0,0,20,20,"fillColor="+couleurs[cmp]);
+            else v1=graphAdapter.insertVertex(graphAdapter.getDefaultParent(),null,utilisateurCentral,0,0,20,20);
+            vertexGraphAdapter.put(utilisateurCentral,v1);
             HashMap<String,Integer>baseRetweeter=baseLink.get(utilisateurCentral);
+
             for(Map.Entry<String,Integer> entry : baseRetweeter.entrySet()) {
-                //On prends 10% des noeuds aléatoirement (afin de ne pas surcharger le graphe)
-                if(Math.random()>0.99)
-                    noeudsFiltre.add( entry.getKey());
+                //On prends 3% des noeuds aléatoirement (afin de ne pas surcharger le graphe)
+                if(Math.random()>0.97) {
+                    Object v2;
+                    if(cmp<cstCouleur)v2=graphAdapter.insertVertex(graphAdapter.getDefaultParent(),null,entry.getKey(),0,0,20,20,"fillColor="+couleurs[cmp]);
+                    else v2=graphAdapter.insertVertex(graphAdapter.getDefaultParent(),null,entry.getKey(),0,0,20,20);
+                    vertexGraphAdapter.put(entry.getKey(),v2);
+                    graphAdapter.insertEdge(graphAdapter.getDefaultParent(),null,""+entry.getValue(),v2, v1);
+                }
             }
-        }
-        grapheClustering=new AsSubgraph(g, noeudsFiltre);
-        //comptage du nombre de noeuds dans la nouveau graphe
-        int cmp=0;
-        for(Object v:grapheClustering.vertexSet()){
             cmp++;
         }
-        System.out.println("Le nouveau graphe contient "+cmp+" noeuds");
+        //Ajout des arrêtes entre retweeter
+        System.out.println("passage arêtes supplémentaires");
+        for(Map.Entry<String,Object> entry : vertexGraphAdapter.entrySet()){
+            String nameRetweeter=entry.getKey();
+            HashMap<String, Integer>baseRetweeter2 = baseLink.get(nameRetweeter);
+            if(baseRetweeter2!=null) {
+                for (Map.Entry<String, Integer> entry2 : baseRetweeter2.entrySet()) {
+                    if (!entry2.getKey().equals(nameRetweeter)&&vertexGraphAdapter.get(entry2.getKey())!=null) {
+                        graphAdapter.insertEdge(graphAdapter.getDefaultParent(),null,""+entry2.getValue(),vertexGraphAdapter.get(entry2.getKey()), vertexGraphAdapter.get(nameRetweeter));
 
-        //grapheClustering.addEdge("LaniieB","Oct_bre",2);
-       // g.addEdge("LaniieB","Oct_bre");
-       // g.setEdgeWeight("LaniieB","Oct_bre", 1);
-        System.out.println(grapheClustering);
-        afficherGraphe(grapheClustering);
-
-
+                    }
+                }
+            }
+        }
+        return graphAdapter;
     }
+
     public void setStyle(JGraphXAdapter graphAdapter){
         Map<String, Object> edgeStyle = new HashMap<String, Object>();
-//edgeStyle.put(mxConstants.STYLE_EDGE, mxConstants.EDGESTYLE_ORTHOGONAL);
 
-        edgeStyle.put(mxConstants.STYLE_SHAPE,    mxConstants.SHAPE_CONNECTOR);
-        edgeStyle.put(mxConstants.STYLE_ENDARROW, mxConstants.ARROW_CLASSIC);
-        edgeStyle.put(mxConstants.STYLE_STROKECOLOR, "#000000");
-        edgeStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
-        edgeStyle.put(mxConstants.STYLE_FONTSIZE, "0");
-        edgeStyle.put(mxConstants.STYLE_LABEL_BACKGROUNDCOLOR, "#ffffff");
         mxStylesheet stylesheet = new mxStylesheet();
-        stylesheet.setDefaultEdgeStyle(edgeStyle);
         Map<String, Object> vertexStyle = new HashMap<String, Object>();
         vertexStyle.put(mxConstants.STYLE_FONTCOLOR, "#000000");
         vertexStyle.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_ELLIPSE);
         vertexStyle.put(mxConstants.STYLE_PERIMETER, mxPerimeter.EllipsePerimeter);
-        //style.put(mxConstants.STYLE_VERTICAL_ALIGN, mxConstants.ALIGN_MIDDLE);
-        //style.put(mxConstants.STYLE_ALIGN, mxConstants.ALIGN_CENTER);
         vertexStyle.put(mxConstants.STYLE_FILLCOLOR, "#CAF7C9");
         vertexStyle.put(mxConstants.STYLE_STROKECOLOR, "#6482B9");
         stylesheet.setDefaultVertexStyle(vertexStyle);
@@ -81,16 +94,15 @@ public class Clustering{
 
 
 
-    public void afficherGraphe(Graph g) {
+    public void afficherGraphe(JGraphXAdapter graphAdapter) {
 
-        JGraphXAdapter<Vertex, DefaultEdge> graphAdapter = new JGraphXAdapter<>(g);
         mxIGraphLayout layout = new  mxFastOrganicLayout(graphAdapter);
-
         //On change le style des arêtes et vertex
         setStyle(graphAdapter);
         layout.execute(graphAdapter.getDefaultParent());
         BufferedImage image = mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null);
-        File imgFile = new File("C:\\Users\\cedri\\Desktop\\graph.png");
+       // File imgFile = new File("C:\\Users\\cedri\\Desktop\\graph.png");
+        File imgFile = new File("graph.png");
         try
         {
             ImageIO.write(image, "PNG", imgFile);
